c1f4d00b0c5054802b243c78cc79757ce6006dd9
gocaspro13 <goncalorg@outlook.pt>
2021-01-13 17:19:32 +0000

    EG-26

---
+++ app/src/main/java/com/eventusgest/createMovementActivity.java
@@ -0,0 +1,14 @@
+package com.eventusgest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createMovementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_movement);
+    }
+}
